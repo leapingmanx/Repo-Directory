0303d53643ac792846a196be48f249dcc53439a4
Unknown <abraxton_94@outlook.com>
2017-12-16 22:47:59 -0600

    Now displays all wanted data.

--- view.js
+++ view.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
-    $.getJSON("https://api.github.com/users/leapingmanx/repos", function(json){
-        $(".repo-list").html(JSON.stringify(json));
-        //testing to make sure api call is successful will change later
+    $.getJSON("https://api.github.com/users/leapingmanx/repos", function (repos){
+        for(i = 0; i < repos.length; i++){
+            var repository = $("<div class='repository'>")
+            var repoName = $("<h3 class='repo-name'><a href='" + repos[i].html_url + "'>"+repos[i].name+"</a></h3>");
+            var repoDescription = $("<p class='repo-description'></p>").text(repos[i].description);
+            //repoDetails to repoStarts is repoDetails
+            var repoDetails = $("<p></p>").text(repos[i].language + "   Forks: " + repos[i].forks + "   Stars: " + repos[i].stargazers_count);
+            $("body").append(repoName, repoDescription, repoDetails);
+        };
     });
 });
