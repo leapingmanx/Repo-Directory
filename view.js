de09ada2252396b740e94c838a5ef63d1f9aef83
Unknown <abraxton_94@outlook.com>
2017-12-19 23:22:12 -0600

    Added CSS styling and final fixes to JS and HTML

--- view.js
+++ view.js
@@ -1,12 +1,23 @@
 $(document).ready(function(){
-    $.getJSON("https://api.github.com/users/leapingmanx/repos", function (repos){
+    fetchRepos('leapingmanx');
+});
+
+function fetchRepos(user){
+    $("#github-username").text(user + "'s Repositories");
+    $.getJSON("https://api.github.com/users/" + user + "/repos", function (repos){
         for(i = 0; i < repos.length; i++){
-            var repository = $("<div class='repository'>")
-            var repoName = $("<h3 class='repo-name'><a href='" + repos[i].html_url + "'>"+repos[i].name+"</a></h3>");
-            var repoDescription = $("<p class='repo-description'></p>").text(repos[i].description);
-            //repoDetails to repoStarts is repoDetails
-            var repoDetails = $("<p></p>").text(repos[i].language + "   Forks: " + repos[i].forks + "   Stars: " + repos[i].stargazers_count);
+            var repoNumber = "repo" + i.toString();
+            var repoName = $("<h3 class='repo-name clear " + repoNumber + "'><a href='" + repos[i].html_url + "'>"+repos[i].name+"</a></h3>");
+            var repoDescription = $("<p class='repo-description clear " + repoNumber +"'></p>").text(repos[i].description);
+            var repoDetails = $("<p class='clear " + repoNumber + "'></p>").text(repos[i].language + "   Forks: " + repos[i].forks + "   Stars: " + repos[i].stargazers_count);
             $("body").append(repoName, repoDescription, repoDetails);
+            $("." + repoNumber).wrapAll("<div class='repo-div'/>")
         };
     });
-});
+};
+
+function update(){
+    var user = document.getElementById("user-search").value;
+    $(".clear").remove();
+    fetchRepos(user);
+}
